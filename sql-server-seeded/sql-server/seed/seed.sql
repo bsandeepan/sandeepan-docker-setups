9007a388c875e3f813e30cd8c2d9f68ab3db11f2
Sandeepan Bhattacharyya <bsandeepan95.work@gmail.com>
2021-05-17 19:06:43 +0530

    added sql-server

---
+++ sql-server-seeded/sql-server/seed/seed.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE TestDB
+SELECT Name from sys.Databases
+GO
+
+USE TestDB
+CREATE TABLE Inventory (id INT, name NVARCHAR(50), quantity INT);
+INSERT INTO Inventory VALUES (1, 'banana', 150); INSERT INTO Inventory VALUES (2, 'orange', 154);
+GO
