ddb33d87dce659d5d74b6b44920c6b34c2ca47d0
Sandeepan Bhattacharyya <bsandeepan95.work@gmail.com>
2022-03-14 21:37:21 +0530

    test commit

--- sql-server-seeded/sql-server/seed/seed.sql
+++ sql-server-seeded/sql-server/seed/seed.sql
@@ -4,5 +4,5 @@ GO
 
 USE TestDB
 CREATE TABLE Inventory (id INT, name NVARCHAR(50), quantity INT);
-INSERT INTO Inventory VALUES (​1, 'banana', 150); INSERT INTO Inventory VALUES (2, 'orange', 154);
+INSERT INTO Inventory VALUES (​1, 'banana', ​150); INSERT INTO Inventory VALUES (2, 'orange', 154);
 GO
